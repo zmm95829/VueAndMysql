e30be1488d8429448729c20479b9e43aea64f16c
zhangmao <zhangmao@boya-triz.com>
2019-05-24 10:30:09 +0800

    能正常编译运行打开

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,9 @@ module.exports = {
   },
   env: {
     browser: true,
-  },
+    node: true,
+    es6: true,
+  }, 
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: "/",
+      name: "HelloWorld",
       component: HelloWorld
-    },    
+    },
     {
-      path: '/OU管理/用户查询',
-      name: 'HelloWorld',
+      path: "/OU管理/用户查询",
+      name: "HelloWorld",
       component: () => "@/views/01vue_element/OU管理/用户管理/List"
     }
   ]
-})
+});
